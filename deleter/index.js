34eb12e162728c11f97564e2004de74a59d79fd5
Florian MARGAINE <florian@margaine.com>
2015-11-16 22:56:12 +0100

    Add the deleter implementation.

--- deleter/index.js
+++ deleter/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Promise = require('bluebird');
+const rmdir = Promise.promisify(require('rimraf'));
 const db = require('../shared/db');
 
 module.exports = function() {
@@ -7,14 +9,33 @@ module.exports = function() {
 };
 
 function getOldFolders() {
+    return Promise.using(db(), function(client) {
+        return client.queryAsync({
+            name: 'get_old_folders',
+            text: "select path from source_folder where created < ( now() - interval '24 hours' )",
+            values: []
+        }).get('rows');
+    });
 }
 
 function deleteFolders(folders) {
     return deleteFolderEntries(folders).then(deleteFilesystemFolders);
 }
 
-function deleteFolderEntries(folder) {
+function deleteFolderEntries(folders) {
+    return folders.map(deleteFolderEntry);
+}
+
+function deleteFolderEntry(folder) {
+    return Promise.using(db(), function(client) {
+        return client.queryAsync({
+            name: 'delete_folder',
+            text: 'delete from source_folder where path = $1',
+            values: [folder]
+        });
+    });
 }
 
 function deleteFilesystemFolders(folders) {
+    return folders.map(rmdir);
 }
