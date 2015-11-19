e09163f0d54338bf52398d5aa06492186d897b69
Florian MARGAINE <florian@margaine.com>
2015-11-19 23:09:00 +0100

    Simplify the entries update.
    Since a package can have multiple versions within the same distribution,
    and it's not possible to account for deletions, we just delete
    everything and start from scratch every time.
    
    Yes, it means there'll be some downtime, but let's hope the server cache
    will catch that.

--- updater/index.js
+++ updater/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const Promise = require('bluebird');
+
 const db = require('../shared/db');
 
 const getSources = require('./sources');
-const createOrUpdateEntries = require('./entries');
+const entries = require('./entries');
+const insertEntries = entries.insert;
+const deleteEntries = entries.delete;
 
 module.exports = function() {
     return Promise.using(db(), function(client) {
@@ -11,10 +15,13 @@ module.exports = function() {
             name: 'get_distributions',
             text: 'select enum_range(null::distribution)',
             values: []
-        }).get('rows').map(function(distribution) {
-            getSources(distribution)
-                .then(createOrUpdateEntries)
+        }).get('rows').get(0).get('enum_range').then(function(range) {
+            return range.match(/{(.+)}/)[1].split(',');
+        }).map(function(distribution) {
+            deleteEntries(distribution)
+                .then(getSources)
+                .then(insertEntries)
                 .done();
-        });
+         });
     });
 };
