0526ef083c8f13cceefa6637f9cb0df8f9f423a1
Florian MARGAINE <florian@margaine.com>
2015-11-21 00:45:40 +0100

    Add retries to download-archive

--- shared/download-archive.js
+++ shared/download-archive.js
@@ -1,10 +1,22 @@
 'use strict';
 
+const log = require('util').log;
+const f = require('util').format;
+
 const Promise = require('bluebird');
 const request = require('request-promise');
 
 Promise.promisifyAll(request);
 
 module.exports = function(url) {
-    return request({ uri: url, encoding: null });
+    // httpredir.debian.org is fairly unreliable, so we retry 5 times.
+    return function r(acc) {
+        return request({ uri: url, encoding: null }).catch(function(err) {
+            if (acc < 5) {
+                log(f('Failed downloading %s, retrying (%d/%d)...', url, acc + 1, 5));
+                return r(++acc);
+            }
+            throw err;
+        });
+    }(0);
 };
