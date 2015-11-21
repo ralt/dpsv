0267ec68868f10be1961d63a02b60d81acfaff31
Florian MARGAINE <florian@margaine.com>
2015-11-21 23:19:23 +0100

    Add an ugly folder view

--- scripts/packages/index.js
+++ scripts/packages/index.js
@@ -93,6 +93,61 @@ function fileRenderer(content) {
 }
 
 function folderRenderer(content) {
+    var folderDiv = document.querySelector('#folder-container');
+    var folderTbody = folderDiv.querySelector('tbody');
+
+    var items = content.sort(function(a, b) {
+        return a.name.charCodeAt(0) - b.name.charCodeAt(0);
+    });
+    var folders = items.filter(function(item) {
+        return item.isFolder;
+    }).map(createFolderElement);
+    var files = items.filter(function(item) {
+        return item.isFile;
+    }).map(createFileElement);
+
+    for (var i = 0; i < folders.length; i++) {
+        folderTbody.appendChild(folders[i]);
+    }
+
+    for (var i = 0; i < files.length; i++) {
+        folderTbody.appendChild(files[i]);
+    }
+
+    folderDiv.hidden = false;
+}
+
+function createElementFolder(item, className) {
+    var tr = document.createElement('tr');
+    tr.className = className;
+    var typeTd = document.createElement('td');
+    typeTd.textContent = item.isFolder ? 'D' : 'F';
+    var nameTd = document.createElement('td');
+    var nameAnchor = document.createElement('a');
+    nameAnchor.textContent = item.name;
+    nameAnchor.href = item.name;
+    nameTd.appendChild(nameAnchor);
+    var modeTd = document.createElement('td');
+    modeTd.textContent = item.mode.toString('8').slice(-3);
+    var creationTd = document.createElement('td');
+    creationTd.textContent = new Date(item.birthtime).toDateString();
+    var modificationTd = document.createElement('td');
+    modificationTd.textContent = new Date(item.mtime).toDateString();
+
+    var tds = [typeTd, nameTd, modeTd, creationTd, modificationTd];
+    tds.forEach(function(td) {
+        tr.appendChild(td);
+    });
+
+    return tr;
+}
+
+function createFolderElement(item) {
+    return createElementFolder(item, 'folder');
+}
+
+function createFileElement(item) {
+    return createElementFolder(item, 'file');
 }
 
 function findSource(source) {
