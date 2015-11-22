99698fd0f9f73bc79e9ee0e4e2f1ff80430a5d1e
Florian MARGAINE <florian@margaine.com>
2015-11-22 14:37:45 +0100

    Handle inexisting original archives

--- api/source-downloader.js
+++ api/source-downloader.js
@@ -25,65 +25,46 @@ const sourceArchiveBaseUrl = 'http://http.debian.net/debian/%s/%s';
 const baseFolder = process.env.SOURCES_FOLDER || '/tmp';
 
 module.exports = function(name, version, directory, archive, debianArchive) {
-    const archiveUrl = f(
-        sourceArchiveBaseUrl, directory, archive
-    );
-    const debianArchiveUrl = f(
-        sourceArchiveBaseUrl, directory, debianArchive
-    );
-
-    const archiveFilename = path.join(baseFolder, archive);
-    const debianArchiveFilename = path.join(baseFolder, debianArchive);
-
     const sourceFolder = path.join(baseFolder, f('%s_%s', name, version));
 
-    return Promise.using(db(), function(client) {
-        return Promise.all([
-            downloadArchive(archiveUrl),
-            downloadArchive(debianArchiveUrl)
-        ]).spread(function(archiveContent, debianArchiveContent) {
-            return [
-                fs.writeFileAsync(
-                    archiveFilename,
-                    archiveContent
-                ),
-                fs.writeFileAsync(
-                    debianArchiveFilename,
-                    debianArchiveContent
-                )
-            ];
-        }).then(function() {
-            // First, extract the original source,
-            // then, the debian archive.
-            return execAsync(f(
-                'mkdir -p %s && tar xf %s --strip-components=1 -C %s',
-                sourceFolder,
-                archiveFilename,
-                sourceFolder
-            ));
-        }).then(function() {
-            return execAsync(f(
-                'tar xf %s -C %s',
-                debianArchiveFilename,
-                sourceFolder
-            ));
-        }).then(function() {
+    return Promise.each([
+        downloadAndExtractArchive(archive, sourceFolder, directory),
+        downloadAndExtractArchive(debianArchive, sourceFolder, directory, true)
+    ]).then(function() {
+        return Promise.using(db(), function(client) {
             return client.queryAsync({
                 name: 'insert_source_folder',
                 text: 'insert into source_folder (path) values($1)',
                 values: [sourceFolder]
             });
-        }).then(function() {
-            // Clean up the archives
-            return [
-                fs.unlinkAsync(archiveFilename),
-                fs.unlinkAsync(debianArchiveFilename)
-            ];
-        }).catch(function(err) {
-            log(err);
         });
     });
 };
+
+function downloadAndExtractArchive(archive, sourceFolder, directory, isDebian) {
+    if (!archive) {
+        return;
+    }
+
+    const archiveUrl = f(sourceArchiveBaseUrl, directory, archive);
+    const archiveFilename = path.join(baseFolder, archive);
+
+    return downloadArchive(archiveUrl).then(function(archiveContent) {
+        return fs.writeFileAsync(archiveFilename, archiveContent);
+    }).then(function() {
+        return execAsync(f(
+            'mkdir -p %s && tar xf %s %s -C %s',
+            sourceFolder,
+            archiveFilename,
+            isDebian ? '' : '--strip-components=1',
+            sourceFolder
+        ));
+    }).then(function() {
+        return fs.unlinkAsync(archiveFilename);
+    }).catch(function(err) {
+        log(err);
+    });
+}
 
 function getOrigVersion(version) {
     return version.replace(/-.*/, '');
