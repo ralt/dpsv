3e62a35cbeefb4742a84bfd36a3e34d5ca889003
Florian MARGAINE <florian@margaine.com>
2015-11-22 18:53:35 +0100

    Add missing dependency in .platform.app.yaml

--- api/renderer.js
+++ api/renderer.js
@@ -49,7 +49,7 @@ function renderFile(realpath) {
         }).catch(function(err) {
             log(err);
             // If we can't run pygmentize on it, just return the raw file
-            return fs.readFileAsync(realpath, 'utf-8');
+            return f('<pre>%s</pre>', fs.readFileAsync(realpath, 'utf-8'));
         });
     });
 }
